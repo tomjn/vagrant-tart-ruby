5210177edf25b8b87fbdc4fe331c340e20ce939b
Tom J Nowell <contact@tomjn.com>
2022-12-21 13:03:47 +0000

    add memory and disk size to config

--- lib/vagrant/tart/config.rb
+++ lib/vagrant/tart/config.rb
@@ -3,7 +3,11 @@ require "vagrant"
 module Vagrant
   module Tart
     class Config < Vagrant.plugin("2", :config)
-      #
+      # @return [Integer] Memory size in MB
+      attr_accessor :memory
+
+      # @return [Integer] Disk storage size in GB
+      attr_accessor :disksize
     end
   end
 end
