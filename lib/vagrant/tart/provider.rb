513dbd4821868b43ec4ca5c52a95d5d016c2da13
Tom J Nowell <contact@tomjn.com>
2023-03-15 18:51:34 +0000

    use the state method from the hyper-v provider

--- lib/vagrant/tart/provider.rb
+++ lib/vagrant/tart/provider.rb
@@ -41,6 +41,31 @@ module Vagrant
       def machine_id_changed
         @driver = Driver.new(@machine.id)
       end
+
+      def state
+        state_id = nil
+        state_id = :not_created if !@machine.id
+
+        if !state_id
+          # Run a custom action we define called "read_state" which does
+          # what it says. It puts the state in the `:machine_state_id`
+          # key in the environment.
+          env = @machine.action(:read_state)
+          state_id = env[:machine_state_id]
+        end
+
+        # Get the short and long description
+        short = state_id.to_s
+        long  = ""
+
+        # If we're not created, then specify the special ID flag
+        if state_id == :not_created
+          state_id = Vagrant::MachineState::NOT_CREATED_ID
+        end
+
+        # Return the MachineState object
+        Vagrant::MachineState.new(state_id, short, long)
+      end
     end
   end
 end
