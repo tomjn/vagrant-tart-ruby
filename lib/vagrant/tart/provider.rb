b8178cbca1648f904fd251b4e0c235255ef0a136
Tom J Nowell <contact@tomjn.com>
2023-03-15 18:52:54 +0000

    support actions in the provider

--- lib/vagrant/tart/provider.rb
+++ lib/vagrant/tart/provider.rb
@@ -42,6 +42,15 @@ module Vagrant
         @driver = Driver.new(@machine.id)
       end
 
+      def action(name)
+        # Attempt to get the action method from the Action class if it
+        # exists, otherwise return nil to show that we don't support the
+        # given action.
+        action_method = "action_#{name}"
+        return Action.send(action_method) if Action.respond_to?(action_method)
+        nil
+      end
+
       def state
         state_id = nil
         state_id = :not_created if !@machine.id
